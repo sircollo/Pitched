453ffef6917f2a95c2659b39a319c832d967b309
Collins Sirwani <sircollo71@gmail.com>
2022-05-06 22:29:26 +0300

    Feat: this will add a custom validator to check if user email or username exists and alerts if its true

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
   email = StringField('E-mail',validators=[DataRequired(),Email()])
@@ -10,3 +11,11 @@ class RegistrationForm(FlaskForm):
   password = PasswordField('Password',validators=[DataRequired(),EqualTo('password_confirm',message='Password mismatch')])
   password_confirm = PasswordField('Confirm Password',validators=[DataRequired])
   submit = SubmitField('Sign Up')
+  
+  def validate_email(self,data_field):
+    if User.query.filter_by(email=data_field.data).first():
+      raise ValidationError('Account with this email exists')
+    
+  def validate_username(self,data_field):
+    if User.query.filter_by(username=data_field.data).first():
+      raise ValidationError('Username taken')
