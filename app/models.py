8b9f8417f9a8e568c3574242ee973545967b1c3f
Collins Sirwani <sircollo71@gmail.com>
2022-05-06 22:10:52 +0300

    Feat: this will import usermixin class, add email column and adds a callback function to retrieve a user

--- app/models.py
+++ app/models.py
@@ -1,11 +1,19 @@
+import email
 from . import db
-from werkzeug import generate_password_hush,check_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer,primary_key = True)
   username = db.Column(db.String(255))
+  email = db.Column(db.String(255),unique=True,index=True)
   role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
   pass_secure = db.Column(db.String(255))
   
@@ -15,7 +23,7 @@ class User(db.Model):
   
   @password.setter
   def password(self,password):
-    self.pass_secure = generate_password_hush(password)
+    self.pass_secure = generate_password_hash(password)
     
   def verify_password(self,password):
     return check_password_hash(self.pass_secure,password)
