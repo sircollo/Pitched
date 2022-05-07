f4d633132f4bd6891c95b5f7091524d8d9ed57f2
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 10:08:17 +0300

    This will add secret key variable configuration for forms security

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pitches'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
   pass
