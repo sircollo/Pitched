bf7a894d3a1e884f5b28c790225da22d5477423d
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 20:10:19 +0300

    This will define the different categories of pitches and queries pitches by category name

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,8 @@ from ..models import User,Pitch
 @main.route('/home')
 def index():
   pitches_list = Pitch.query.all()
-  return render_template('index.html',pitches_list=pitches_list)
-
-
-
+  business_pitch = Pitch.query.filter_by(category = 'Business').all()
+  technology_pitch = Pitch.query.filter_by(category = 'Technology').all()
+  interview_pitch = Pitch.query.filter_by(category = 'Interview').all()
+  farming_pitch = Pitch.query.filter_by(category='Farming').all()
+  return render_template('index.html',pitches_list=pitches_list,business_pitch=business_pitch,interview_pitch=interview_pitch,technology_pitch=technology_pitch, farming_pitch=farming_pitch)
