dbe94e87fcf1968b238241e1111749093d5d6bb6
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 16:52:49 +0300

    This will add database test configurations

--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 import os
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pitches'
+  # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pitches'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
   pass
 
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pitches_test'
+
 class DevConfig(Config):
+  '''
+  Dev config child class
+  '''
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://collo:1234@localhost/pitches'
   DEBUG = True
   
 config_options = {
   'development':DevConfig,
-  'production':ProdConfig
+  'production':ProdConfig,
+  'test':TestConfig
 }
