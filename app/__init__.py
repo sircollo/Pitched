7115c640aeefbcebeabb099ff064e0daf21e3649
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 10:24:22 +0300

    This will register auth blueprint and import login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
@@ -19,6 +22,7 @@ def create_app(config_name):
   app.config.from_object(config_options[config_name])
   
   #initialize flask extensions
+  bootstrap.init_app(app)
   db.init_app(app)
   login_manager.init_app(app)
   
