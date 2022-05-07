a2b18bc1c78967f243b74ca5fbc4470e9bc9c4f2
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 16:58:31 +0300

    This will define the home route, and create a variable to query pitches list and render index.html page

--- app/main/views.py
+++ app/main/views.py
@@ -3,10 +3,12 @@ from . import main
 from flask_login import login_required
 from flask_login import login_required
 from .. import db
+from ..models import User,Pitch
 
 @main.route('/home')
 def index():
-  return render_template('index.html')
+  pitches_list = Pitch.query.all()
+  return render_template('index.html',pitches_list=pitches_list)
 
 
 
