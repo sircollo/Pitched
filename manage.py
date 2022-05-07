e8db93f73f94eb52e3a96168df59f0650b3910ad
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 16:53:53 +0300

    This will create a variable for pitch dictionary

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User,Role,Pitch
 from flask_migrate import Migrate,MigrateCommand
 #create app instances
 app = create_app('development')
@@ -20,7 +20,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-  return dict(app=app, db = db, User=User, Role= Role)
+  return dict(app=app, db = db, User=User, Role= Role, Pitch = Pitch)
 
 if __name__=='__main__':
   manager.run()
