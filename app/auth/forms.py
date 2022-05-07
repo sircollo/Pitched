d073ee852b93f52719bbb6e8dbd9002a5aca4576
Collins Sirwani <sircollo71@gmail.com>
2022-05-07 10:19:52 +0300

    This will add wtf registration and login forms and validates user input

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,17 @@
-import email
+# import email
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Email,EqualTo,length
 from ..models import User
 from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
   email = StringField('E-mail',validators=[DataRequired(),Email()])
-  username = StringField('Username',validators=[DataRequired])
-  password = PasswordField('Password',validators=[DataRequired(),EqualTo('password_confirm',message='Password mismatch')])
-  password_confirm = PasswordField('Confirm Password',validators=[DataRequired])
-  submit = SubmitField('Sign Up')
-  
+  username = StringField('Username',validators=[DataRequired(),length(min=4,max=15)])
+  password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+  password_confirm = PasswordField('Confirm Password',validators=[DataRequired()])
+  submit = SubmitField(label='Sign Up')
+    
   def validate_email(self,data_field):
     if User.query.filter_by(email=data_field.data).first():
       raise ValidationError('Account with this email exists')
@@ -19,3 +19,10 @@ class RegistrationForm(FlaskForm):
   def validate_username(self,data_field):
     if User.query.filter_by(username=data_field.data).first():
       raise ValidationError('Username taken')
+    
+class LoginForm(FlaskForm):
+  email = StringField('E-mail',validators=[DataRequired(),Email()])
+  password = PasswordField('Password',validators =[DataRequired()])
+  remember = BooleanField('Remember me')
+  submit = SubmitField('Login')
+
