3ffbb498ce0326c81aa7a02af0a3c5922f403272
Collins Sirwani <sircollo71@gmail.com>
2022-05-08 12:38:51 +0300

    This will add a logout function and return user to main page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_required, login_user,logout_user
 from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
@@ -29,3 +29,10 @@ def register():
       return redirect(url_for('auth.login'))
       title= "New Account"
   return render_template('auth/register.html',registration_form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main.index"))
