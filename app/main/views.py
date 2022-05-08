6a24d1f8e9c38438e7a244e3d55f3daaaebd8979
Collins Sirwani <sircollo71@gmail.com>
2022-05-08 13:20:04 +0300

    This will define route for user to update profile

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required
 from flask_login import login_required,current_user
 from .. import db
 from ..models import User,Pitch,Upvote,Downvote
-from .forms import PitchForm
+from .forms import PitchForm,UpdateProfile
 
 @main.route('/home')
 def index():
@@ -40,4 +40,24 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
 
