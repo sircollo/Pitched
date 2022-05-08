a3fc78f2cd0d68f1171e7e1c28a50a54972d9a96
Collins Sirwani <sircollo71@gmail.com>
2022-05-08 13:39:31 +0300

    This will define a route to process uploads form submission, save method to save files in the application

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template,redirect,url_for,abort
+from flask import render_template,redirect,url_for,abort,request
 from . import main
 from flask_login import login_required
 from flask_login import login_required,current_user
-from .. import db
+from .. import db,photos
 from ..models import User,Pitch,Upvote,Downvote
 from .forms import PitchForm,UpdateProfile
 
@@ -61,3 +61,13 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form =form)
 
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
