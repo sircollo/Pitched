bfdb9f5696ce105acc3b6a63f60353a358842246
Collins Sirwani <sircollo71@gmail.com>
2022-05-08 13:56:41 +0300

    This will import mail message function and gets called in the register view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,6 +5,7 @@ from . import auth
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/', methods=['GET','POST'])
 def login():
@@ -26,6 +27,8 @@ def register():
       user = User(email = form.email.data, username = form.username.data, password = form.password.data)
       db.session.add(user)
       db.session.commit()
+      
+      mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
       return redirect(url_for('auth.login'))
       title= "New Account"
   return render_template('auth/register.html',registration_form=form)
